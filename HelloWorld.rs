330dfcd32f233179443a0d13f2f1c97d52314109
Yash Chavan <chavanyash127@gmail.com>
2024-05-19 11:24:48 +0530

    lets start Hello World in rust

---
+++ HelloWorld.rs
@@ -0,0 +1,15 @@
+fn main(){ 
+    println!("Hello world");
+    print!("hello Rust Users");
+  }
+  
+  
+  /* 
+  
+  1. fn is keyword for initialize function.
+  2. main is function name.
+  3. compiler starts reading code from main function.
+  4. println! is a function and use for printing content then add new line.
+  5. print! is a function and use for printing content.
+     
+  */
