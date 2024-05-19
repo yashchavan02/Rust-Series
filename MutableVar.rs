9891c6e107939c90c81f127dacd8f84f5bdfcabc
Yash Chavan <chavanyash127@gmail.com>
2024-05-19 15:54:41 +0530

    mut keyword , mutable variable in rust

---
+++ MutableVar.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x: i32 =1;
+    x+=2;
+    assert_eq!(x,3);
+    println!("Program is run ");
+  }
+
+/*
+
+1.here "mut" is a keyword use for making variables mutable
+2.by default variables are immutable.
+3. [x+=2] is simply represent [x=x+2]
+
+*/
