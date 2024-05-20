8a32586ea51b3a773a54c159c5f49461195fe08d
Yash Chavan <chavanyash127@gmail.com>
2024-05-20 16:32:43 +0530

    function and functional calling in rust

---
+++ FunctionCall.rs
@@ -0,0 +1,19 @@
+fn main(){
+    say();
+}
+fn say(){
+  let x: &str ="Hello";
+  print!("{} Coders.",x);
+}
+
+/*
+
+1.fuction is defined as a block of code that performs some operations.
+2.their have three steps: 1) function declaration
+                          2) function definition
+                          3) function call
+3.syntax : [fn] keyword [function name] (parameters/arguments){prosses};
+4.say() is a function that simply prints Hello Coders. 
+5.always function call in main function and declare outside main function.
+
+*/
