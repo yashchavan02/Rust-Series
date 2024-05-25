6292882a859a4b1d28d120dc43c20c8d2aa14352
Yash Chavan <chavanyash127@gmail.com>
2024-05-25 12:15:36 +0530

    Conditional Approach in rust

---
+++ ConditionalApproach.rs
@@ -0,0 +1,29 @@
+fn main() { 
+
+	let n = 10; 
+
+	if n > 10 { 
+		println!("Greater than 10"); 
+  }
+  
+  else if n==10 { 
+		println!("Equal to 10"); 
+  }
+  
+  else { 
+		println!("Smaller than 10"); 
+
+	} 
+} 
+
+
+/*
+
+1.The fn main() function is the entry point of the program.
+2.Inside the main() function, a variable n is declared and assigned the value 10.
+3.The [if] statement checks if n is greater than 10. If the condition is true, it prints "Greater than 10".
+4.The [else if] statement checks if n is equal to 10. If the condition is true, it prints "Equal to 10".
+5.If neither the if nor the else if condition is met, the [else] block is executed, and it prints "Smaller than 10".
+
+
+*/
