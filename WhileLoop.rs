f0c92adf00e59b2bc154d7c2770a08b1a126b1b0
Yash Chavan <chavanyash127@gmail.com>
2024-05-30 23:29:31 +0530

    while loops in rust

---
+++ WhileLoop.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut num: i32 = 1;
+      while num!=10{
+      println!("{}",num);
+      num=num+1;
+    }
+  }
+
+
+  
+
+/*
+
+1.In this program, the main function initializes a mutable integer variable num with a value of 1. The while loop then checks if num is not equal to 10. If the condition is true, it prints the current value of num and increments num by 1. This process continues until num becomes 10.
+2.The while loop in Rust is similar to the while loop in other programming languages, allowing you to repeatedly execute a block of code as long as a specified condition remains true. In this case, the loop continues until num reaches 10, printing each number in the process.
+
+
+*/  
