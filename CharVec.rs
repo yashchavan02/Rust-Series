44c32cda0a6c4caadb92699681ce2dd58b504b31
Yash Chavan <chavanyash127@gmail.com>
2024-06-05 22:58:57 +0530

    Character vector & it's opperation and len() function

--- CharVec.rs
+++ CharVec.rs
@@ -18,3 +18,23 @@ fn main() {
 
   println!("String length is {}",s.len());
 }
+
+
+/*
+
+
+
+1.A mutable vector a of characters is declared and initialized with a list of characters.
+2.The push method is used to add the character 'i' to the end of the vector.
+3.The remove method is used to remove the first element ('z') from the vector.
+4.A for loop is used to iterate over a reference to the vector a and print each character.
+5.After the loop, the pop method is used to remove the last element ('i') from the vector.
+6.The into_iter method is used to create an iterator over the vector a.
+7.The collect method is used to convert the iterator into a String.
+8.The resulting string is printed, showing the conversion of the character vector to a string.
+9.Finally, the length of the string is printed using the len() method.
+
+
+
+
+*/
