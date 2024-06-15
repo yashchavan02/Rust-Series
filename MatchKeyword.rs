1221d79f5df3b2a4f41d435662b7705177aa34f1
Yash Chavan <chavanyash127@gmail.com>
2024-06-15 23:23:58 +0530

    'match' keyword in rust

---
+++ MatchKeyword.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let month = "Apr";
+    
+    match month 
+      {
+        "Jan"=>println!("जानेवारी"),
+        "Feb"=>println!("फेब्रुवारी"),
+        "Mar"=>println!("मार्च"),
+        "Apr"=>println!("Happy Birthday YC"),
+        "May"=>println!("मे"),
+        "Jun"=>println!("जून"),
+        "Jul"=>println!("जुलै"),
+        "Aug"=>println!("ऑगस्ट"),
+        "Sep"=>println!("सप्टेंबर"),
+        "Oct"=>println!("ऑक्टोबर"),
+        "Nov"=>println!("नोव्हेंबर"),
+        "Dec"=>println!("डिसेंबर"),
+        _ => println!("code is run."),
+      }
+    
+}
+  
+
+
+
+
+
+/*
+
+
+
+1.The main function is defined, which is the entry point of the program.
+2.Inside the main function, a variable month is declared and assigned the value "Apr".
+3.The match expression is used to compare the month variable with different string patterns.
+4.If the month matches "Jan", "जानेवारी" is printed.
+5.If the month matches "Feb", "फेब्रुवारी" is printed.
+6.If the month matches "Mar", "मार्च" is printed.
+7.If the month matches "Apr", "Happy Birthday YC" is printed.
+8.If the month matches "May", "मे" is printed.
+9.If the month matches "Jun", "जून" is printed.
+10.If the month matches "Jul", "जुलै" is printed.
+11.If the month matches "Aug", "ऑगस्ट" is printed.
+12.If the month matches "Sep", "सप्टेंबर" is printed.
+13.If the month matches "Oct", "ऑक्टोबर" is printed.
+14.If the month matches "Nov", "नोव्हेंबर" is printed.
+15.If the month matches "Dec", "डिसेंबर" is printed.
+16.If none of the patterns match, "code is run." is printed.
+
+
+
+*/  
