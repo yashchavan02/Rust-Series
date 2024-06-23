0ddd7d98ae9008138ece076f69f99942d531f162
Yash Chavan <chavanyash127@gmail.com>
2024-06-23 11:00:47 +0530

    add git packeges

---
+++ src/rustpackeges/target/debug/deps/rustpac-0f50fe785eac5b0c.d
@@ -0,0 +1,5 @@
+C:\Users\chava\OneDrive\Desktop\.vscode\rustx\rustpac\target\debug\deps\librustpac-0f50fe785eac5b0c.rmeta: src\main.rs
+
+C:\Users\chava\OneDrive\Desktop\.vscode\rustx\rustpac\target\debug\deps\rustpac-0f50fe785eac5b0c.d: src\main.rs
+
+src\main.rs:
---
+++ src/rustpackeges/target/debug/deps/rustpac-ab4e94faf033a8c4.d
@@ -0,0 +1,5 @@
+C:\Users\chava\OneDrive\Desktop\.vscode\rustx\rustpac\target\debug\deps\librustpac-ab4e94faf033a8c4.rmeta: src\main.rs
+
+C:\Users\chava\OneDrive\Desktop\.vscode\rustx\rustpac\target\debug\deps\rustpac-ab4e94faf033a8c4.d: src\main.rs
+
+src\main.rs:
