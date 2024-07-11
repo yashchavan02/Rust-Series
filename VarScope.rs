c7e699d9cf870e579ca307a9be089ceefe3e3a90
Yash Chavan <chavanyash127@gmail.com>
2024-07-11 19:09:52 +0530

    Correct the explaination and code of VarScope.rs

--- VarScope.rs
+++ VarScope.rs
@@ -2,16 +2,20 @@ fn main() {
   let x : i32 = 20;
 
   {
-    let x: i32 = 10;
+    let y : i32 = 10;
     println!("The value of scope x is {}",x);
+    println!("The value of scope y is {}",y);
   }
     println!("The value of x is {}", x);
+    // println!("The value of y is {}", y);
 }
+
 
 /*
 
 1.in scope original value of x is 10 .
-2.when we print the value of x again (out of scope) the value is 20.
-3.the validity of variable is only in their scope.
+2.when we print the value of y again (In line no. 10) it trow an error -> error[E0425]: cannot find value `y` in this scope.
+3.variable y is only valid in block from line no. 4 to 8.
+4.the validity of variable is only in their scope.
 
 */
